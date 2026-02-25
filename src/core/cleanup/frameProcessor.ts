80bd2a4795b4ca14349e2af5eb17901d23b3618f
Forgata <gombwadivine@gmail.com>
2026-02-25 16:48:49 +0200

    fix: adjust peak threshold and probability for voice activity detection in processFrame function

--- src/core/cleanup/frameProcessor.ts
+++ src/core/cleanup/frameProcessor.ts
@@ -1,5 +1,3 @@
-import path from "node:path";
-import { saveID } from "../pipeline/idStorage.js";
 import { collectMFCC } from "../pipeline/mfccCollector.js";
 import { extractMFCC } from "../pipeline/mfccExtractor.js";
 import { calculateVoiceprint } from "../pipeline/voiceprint.js";
@@ -12,12 +10,12 @@ const MAX_SPEECH_FRAMES = 150;
 
 export async function processFrame(frame: Int16Array) {
   const peak = Math.max(...frame.map(Math.abs));
-  if (peak > 900) {
+  if (peak > 950) {
     const normalisedFrame = normaliseInt16(frame);
 
     const probability = await cobraVAD(normalisedFrame);
 
-    if (probability > 0.65) {
+    if (probability > 0.7) {
       console.log("Voice activity detected...");
 
       if (speechBuffer.length >= MAX_SPEECH_FRAMES)
@@ -29,9 +27,6 @@ export async function processFrame(frame: Int16Array) {
       const fullMatrix = collectMFCC(features);
 
       if (fullMatrix) return calculateVoiceprint(fullMatrix);
-
-      // console.log("Windowed sample [0]:", frameWindow[0]);
-
       speechBuffer.push(frameWindow);
     }
   }
