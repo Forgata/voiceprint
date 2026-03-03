c4170ff3dca2d41f8de3c79855655effac17aa31
Forgata <gombwadivine@gmail.com>
2026-03-04 00:15:33 +0200

    refactor: update loadStoredVoicePrint function to accept dynamic file names

--- src/core/scoring/idManager.ts
+++ src/core/scoring/idManager.ts
@@ -3,8 +3,11 @@ import path from "node:path";
 import type { VoicePrint } from "../pipeline/idStorage.js";
 const file = path.join(process.cwd(), "voiceprint.json");
 
-export function loadStoredVoicePrint(filepath: string = file) {
-  if (!fs.existsSync(filepath)) throw new Error("voice print file not found");
-  const rawData = fs.readFileSync(filepath, "utf-8");
+export function loadStoredVoicePrint(
+  fileName: string = "voiceprint.print.json",
+) {
+  const file = path.join(process.cwd(), `${fileName}.print.json`);
+  if (!fs.existsSync(file)) throw new Error("voiceprint file not found");
+  const rawData = fs.readFileSync(file, "utf-8");
   return JSON.parse(rawData) as VoicePrint;
 }
