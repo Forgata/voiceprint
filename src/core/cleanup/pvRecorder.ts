7f9c28c0e9631e83cee3ce0fb2293c35c0b39992
Forgata <gombwadivine@gmail.com>
2026-03-04 02:47:51 +0200

    refactor: enhance pvRecord function for improved print file name handling and logging

--- src/core/cleanup/pvRecorder.ts
+++ src/core/cleanup/pvRecorder.ts
@@ -7,21 +7,26 @@ import chalk from "chalk";
 
 export async function pvRecord() {
   const mode = process.argv[2] === "enroll" ? "enroll" : "verify";
-  const printFileName = process.argv[3];
+  const printFileName = process.argv[3]?.trim().toLowerCase();
 
   const devices = PvRecorder.getAvailableDevices();
-  devices.forEach((device, index) => console.log(`${index}: ${device}`));
 
   const frameLength = 512;
   const recorder = new PvRecorder(frameLength, -1);
   recorder.start();
 
-  console.log(`recording started with device: ${devices[0]} | default device`);
-  console.log(`--- MODE: ${mode.toUpperCase()} ---`);
+  console.log("Recording started with default device: ", devices[0]);
+  console.log(chalk.bgRedBright(`--- MODE: ${mode.toUpperCase()} ---`));
 
   if (!printFileName)
     console.log(
       `${chalk.yellowBright("No print file name was provided as an argument.")}\n${chalk.blueBright("Voiceprint will default to voiceprint.print.json")}`,
+    );
+  else
+    console.log(
+      mode === "enroll"
+        ? chalk.yellow(`Enrolling voice print to ${printFileName}.print.json`)
+        : `Verifying ${printFileName}.print.json`,
     );
 
   try {
@@ -34,10 +39,13 @@ export async function pvRecord() {
       if (mode === "enroll") {
         saveID(resultPrint, ...(printFileName ? [printFileName] : []));
       } else {
-        const stored = loadStoredVoicePrint();
+        const stored = !printFileName
+          ? loadStoredVoicePrint()
+          : loadStoredVoicePrint(printFileName);
         const score = cosineSimilarity(resultPrint, stored.voiceprint);
 
-        console.log(`\nSimilarity Score: ${(score * 100).toFixed(2)}%`);
+        console.log(`\nSimilarity Score: ${(score * 100).toFixed(3)}%`);
+
         console.log(score > 0.85 ? "✅ ACCESS GRANTED" : "❌ ACCESS DENIED");
       }
       break;
